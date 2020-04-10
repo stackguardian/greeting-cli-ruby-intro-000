ec5e065fda5eadc88eeb043623f63a47139e6e79
Muhtasim Ferdous <mferdous98@aol.com>
2020-04-10 02:06:54 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,10 @@
 # code the #greeting method here!
+def greeting(name)
+  puts "Hello, #{name}. It's nice to meet you."
+  Sally
+end
+
+puts "Hi! Welcome to the wonderful world of Ruby programming."
+puts "Please enter your name so that we can greet you more personally:"
+name = gets.strip
+greeting(name)
